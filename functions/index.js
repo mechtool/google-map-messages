2e6bd551ff90e209a159f0c4fc6ea55c65cf4525
Dell <farros@rambler.ru>
2018-01-17 11:33:55 +0300

    first commit

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,18 @@
-const functions = require("firebase-functions") ;
 const fireAdmin = require('firebase-admin');
+const functions = require("firebase-functions") ;
+const express = require("express") ;
+const cors = require('cors');
+
 fireAdmin.initializeApp({
   credential : fireAdmin.credential.applicationDefault(),
   apiKey: "AIzaSyANdoXC0pXgwavYarGIt_E2ptScC-QsulU",
   authDomain: "gis-message-bdccb.firebaseapp.com",
   databaseURL: "https://gis-message-bdccb.firebaseio.com",
   projectId: "gis-message-bdccb",
-  storageBucket: "",
+  storageBucket: "gis-message-bdccb.appspot.com",
   messagingSenderId: "226145250276",
 });
-const express = require("express") ;
-const cors = require('cors');
+
 const jsonData = JSON.parse(JSON.stringify([{"_id" : { "$oid" : "5770f18dec2c5aec10d81a0c" }, "date" : "01.08.2013", "body" : "Что за #дебил придумал устраивать ипподром на главной площади страны?", "lat" : "55.7542645", "long" : "37.62019157", "name" : "Aleksey S.", "center" : 1, "dateNumber" : 1375315200000 }
 ]));//JSON.parse(require('./lib/messageDate-21-06-17.json'));
 /* Express */
